b5c9a4f97e246a0e420baba275fa95a4de80b87e
Pablo de la Peña <p@shedcollective.org>
2015-11-18 17:48:46 +0000

    [WIP] Work on Driver

--- src/Stripe.php
+++ src/Stripe.php
@@ -1,7 +1,59 @@
 <?php
 
+/**
+ * Stripe payment Driver
+ *
+ * @package     Nails
+ * @subpackage  driver-order-payment-stripe
+ * @category    Driver
+ * @author      Nails Dev Team
+ * @link
+ */
+
 namespace Nails\OrderPayment\Driver;
 
-class Stripe implements \Nails\OrderPayment\Interfaces\Driver
+use Nails\OrderPayment\Driver\Base;
+
+class Stripe extends Base
 {
+    protected $sLabel = 'Stripe';
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Returns the driver's configurable options
+     * @return array
+     */
+    public function getConfig(){
+        return array();
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Configures the driver using the saved values from getConfig();
+     */
+    public function setConfig($aConfig){
+        return $this;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Take a payment
+     * @return boolean
+     */
+    public function charge(){
+        return true;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Issue a refund for a payment
+     * @return boolean
+     */
+    public function refund(){
+        return true;
+    }
 }
