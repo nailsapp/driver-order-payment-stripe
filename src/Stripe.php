3cfd1a60f7f9b13bb892424e6ebf653d8ac0e14d
Pablo de la Peña <p@shedcollective.org>
2016-01-12 12:37:04 +0000

    [WIP] Updated in line with progression of module

--- src/Stripe.php
+++ src/Stripe.php
@@ -12,28 +12,32 @@
 
 namespace Nails\Invoice\Driver;
 
+use Nails\Factory;
 use Nails\Invoice\Driver\Base;
+use Nails\Invoice\Exception\DriverException;
 
 class Stripe extends Base
 {
     protected $sLabel = 'Stripe';
 
-    // --------------------------------------------------------------------------
-
-    /**
-     * Returns the driver's configurable options
-     * @return array
-     */
-    public function getConfig(){
-        return array();
-    }
+    //  API Keys
+    protected $sKeyTestSecret;
+    protected $sKeyTestPublic;
+    protected $sKeyLiveSecret;
+    protected $sKeyLivePublic;
 
     // --------------------------------------------------------------------------
 
     /**
-     * Configures the driver using the saved values from getConfig();
+     * Configures the driver
+     * @return object
      */
-    public function setConfig($aConfig){
+    public function setConfig($aConfig) {
+        parent::setConfig($aConfig);
+        $this->sKeyTestSecret = !empty($aConfig['key_test_secret']) ? : null;
+        $this->sKeyTestPublic = !empty($aConfig['key_test_public']) ? : null;
+        $this->sKeyLiveSecret = !empty($aConfig['key_live_secret']) ? : null;
+        $this->sKeyLivePublic = !empty($aConfig['key_live_public']) ? : null;
         return $this;
     }
 
@@ -41,19 +45,57 @@ class Stripe extends Base
 
     /**
      * Take a payment
-     * @return boolean
+     * @return \Nails\Invoice\Model\ChargeResponse
      */
-    public function charge(){
-        return true;
+    public function charge($aCard, $iAmount, $sCurrency) {
+
+        $oResponse = Factory::factory('ChargeResponse', 'nailsapp/module-invoice');
+
+        try {
+
+            if (ENVIRONMENT === 'PRODUCTION') {
+
+                \Stripe\Stripe::setApiKey($this->sKeyLiveSecret);
+
+            } else {
+
+                \Stripe\Stripe::setApiKey($this->sKeyTestSecret);
+            }
+
+            $oStripeResponse = \Stripe\Charge::create(
+                array(
+                    'amount'   => $iAmount,
+                    'currency' => $sCurrency,
+                    'source'   => array(
+                        'object'    => 'card',
+                        'name'      => $aCard['name'],
+                        'number'    => $aCard['number'],
+                        'exp_month' => $aCard['exp_month'],
+                        'exp_year'  => $aCard['exp_year'],
+                        'cvc'       => $aCard['cvc']
+                    )
+                )
+            );
+
+            dumpanddie($oStripeResponse);
+
+        } catch(\Exception $e) {
+
+            throw new DriverException($e->getMessage(), $e->getCode());
+        }
+
+        return $oResponse;
     }
 
     // --------------------------------------------------------------------------
 
     /**
      * Issue a refund for a payment
-     * @return boolean
+     * @return \Nails\Invoice\Model\RefundResponse
      */
-    public function refund(){
-        return true;
+    public function refund() {
+
+        $oResponse = Factory::factory('RefundResponse', 'nailsapp/module-invoice');
+        return $oResponse;
     }
 }
