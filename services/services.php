372bd8967158d350b487d02e999924ebb178b968
Pablo de la Peña <p@shedcollective.org>
2016-06-17 13:20:32 +0100

    Correcting namespaces

--- services/services.php
+++ services/services.php
@@ -3,17 +3,17 @@
 return array(
     'models' => array(
         'Customer' => function () {
-            if (class_exists('App\Invoice\Driver\Payment\Stripe\Customer')) {
-                return new App\Invoice\Driver\Payment\Stripe\Customer();
+            if (class_exists('App\Invoice\Driver\Payment\Stripe\Model\Customer')) {
+                return new App\Invoice\Driver\Payment\Stripe\Model\Customer();
             } else {
-                return new Nails\Invoice\Driver\Payment\Stripe\Customer();
+                return new Nails\Invoice\Driver\Payment\Stripe\Model\Customer();
             }
         },
         'Source' => function () {
-            if (class_exists('App\Invoice\Driver\Payment\Stripe\Source')) {
-                return new App\Invoice\Driver\Payment\Stripe\Source();
+            if (class_exists('App\Invoice\Driver\Payment\Stripe\Model\Source')) {
+                return new App\Invoice\Driver\Payment\Stripe\Model\Source();
             } else {
-                return new Nails\Invoice\Driver\Payment\Stripe\Source();
+                return new Nails\Invoice\Driver\Payment\Stripe\Model\Source();
             }
         }
     )
